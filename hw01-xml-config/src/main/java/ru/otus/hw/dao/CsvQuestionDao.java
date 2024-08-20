368379b180d9d296c13771e8fbd07ef4aa5c3e01
Vlad Sizion <vladislav.sizion@mail.ru>
2024-08-20 23:08:26 +0300

    hw01 sout CSV
    Signed-off-by: Vlad Sizion <vladislav.sizion@mail.ru>

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -11,7 +11,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,20 +19,14 @@ public class CsvQuestionDao implements QuestionDao {
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName())) {
+        try (InputStream inputStream = getClass()
+                .getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName())) {
             if (inputStream == null) {
                 throw new QuestionReadException("File is empty: " + fileNameProvider.getTestFileName());
             }
-
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-            return  (new CsvToBeanBuilder<QuestionDto>(bufferedReader)
+            return (new CsvToBeanBuilder<QuestionDto>(bufferedReader)
                     .withSeparator(';')
                     .withSkipLines(1)
                     .withType(QuestionDto.class)
@@ -43,7 +36,7 @@ public class CsvQuestionDao implements QuestionDao {
                     .map(QuestionDto::toDomainObject).toList());
 
         } catch (IOException e) {
-            throw new QuestionReadException("Something went wrong while reading",e);
+            throw new QuestionReadException("Something went wrong while reading", e);
         }
     }
 }
--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,13 +1,13 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
-import ru.otus.hw.dao.CsvQuestionDao;
 import ru.otus.hw.dao.QuestionDao;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
+
     private final QuestionDao questionDao;
 
     @Override
@@ -15,7 +15,6 @@ public class TestServiceImpl implements TestService {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         // Получить вопросы из дао и вывести их с вариантами ответов
-
         ioService.printLine(questionDao.findAll().toString());
     }
 }
