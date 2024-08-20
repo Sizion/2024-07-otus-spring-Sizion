1a04e0a60cea2cafe9dfce62bd7947541210b57a
Vlad Sizion <vladislav.sizion@mail.ru>
2024-08-20 23:03:28 +0300

    hw01 sout CSV
    Signed-off-by: Vlad Sizion <vladislav.sizion@mail.ru>

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,9 +1,16 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +25,25 @@ public class CsvQuestionDao implements QuestionDao {
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
 
-        return new ArrayList<>();
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName())) {
+            if (inputStream == null) {
+                throw new QuestionReadException("File is empty: " + fileNameProvider.getTestFileName());
+            }
+
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            return  (new CsvToBeanBuilder<QuestionDto>(bufferedReader)
+                    .withSeparator(';')
+                    .withSkipLines(1)
+                    .withType(QuestionDto.class)
+                    .build()
+                    .parse()
+                    .stream()
+                    .map(QuestionDto::toDomainObject).toList());
+
+        } catch (IOException e) {
+            throw new QuestionReadException("Something went wrong while reading",e);
+        }
     }
 }
