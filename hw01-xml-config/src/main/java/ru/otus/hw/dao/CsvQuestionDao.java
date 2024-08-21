abb7aae4e938a35cf93e10634f0db81fa77902b1
Vlad Sizion <vladislav.sizion@mail.ru>
2024-08-21 13:04:17 +0300

    hw01 sout CSV fixed
    Signed-off-by: Vlad Sizion <vladislav.sizion@mail.ru>

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,39 +1,44 @@
 package ru.otus.hw.dao;
 
 import com.opencsv.bean.CsvToBeanBuilder;
+import lombok.Cleanup;
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
 import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Objects;
+
+
 
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
+
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
+        String fileName = fileNameProvider.getTestFileName();
+
         try (InputStream inputStream = getClass()
-                .getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName())) {
-            if (inputStream == null) {
-                throw new QuestionReadException("File is empty: " + fileNameProvider.getTestFileName());
+                .getClassLoader().getResourceAsStream(fileName)) {
+            if (Objects.isNull(inputStream)) {
+                throw new QuestionReadException("File is empty: " + fileName);
             }
-            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-            return (new CsvToBeanBuilder<QuestionDto>(bufferedReader)
+            @Cleanup InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            return new CsvToBeanBuilder<QuestionDto>(inputStreamReader)
                     .withSeparator(';')
                     .withSkipLines(1)
                     .withType(QuestionDto.class)
                     .build()
                     .parse()
                     .stream()
-                    .map(QuestionDto::toDomainObject).toList());
+                    .map(QuestionDto::toDomainObject).toList();
 
         } catch (IOException e) {
             throw new QuestionReadException("Something went wrong while reading", e);
--- hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -2,6 +2,8 @@ package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
@@ -15,6 +17,14 @@ public class TestServiceImpl implements TestService {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         // Получить вопросы из дао и вывести их с вариантами ответов
-        ioService.printLine(questionDao.findAll().toString());
+        //      ioService.printLine(questionDao.findAll().toString());
+
+        for (Question question : questionDao.findAll()) {
+            ioService.printLine("Question - " + question.text());
+            for (Answer answer : question.answers()) {
+                ioService.printFormattedLine("  answer - " + answer.text());
+            }
+            ioService.printLine("----------------------------------------");
+        }
     }
 }
